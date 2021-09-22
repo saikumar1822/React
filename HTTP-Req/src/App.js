07bc2e66dedbb4e33808768f342a250cbd031c1e
Sai Kumar Yaddalapoori <saikumar.yadda@hcl.com>
2021-09-22 10:48:29 +0530

    folders added

---
+++ HTTP-Req/src/App.js
@@ -0,0 +1,108 @@
+import React, { useState, useEffect, useCallback } from 'react';
+
+import MoviesList from './components/MoviesList';
+import AddMovie from './components/AddMovie';
+import './App.css';
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchMoviesHandler = useCallback(async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch('https://react-http-43dd7-default-rtdb.firebaseio.com/movies.json');
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+
+      const data = await response.json();
+
+      const loadedMovies =[];
+      for(const key in data){
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+      // const transformedMovies = data.results.map((movieData) => {
+      //   return {
+      //     id: movieData.episode_id,
+      //     title: movieData.title,
+      //     openingText: movieData.opening_crawl,
+      //     releaseDate: movieData.release_date,
+      //   };
+      // });
+      setMovies(loadedMovies);
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+
+  async function addMovieHandler(movie) {
+   const response= await fetch('https://react-http-43dd7-default-rtdb.firebaseio.com/movies.json',{
+     method: 'POST',
+     body: JSON.stringify(movie),
+     headers: {
+       'Content-Type': 'application/json'
+     }
+   });
+   const data = await response.json();
+   console.log(data);
+  }
+
+  let content = <p>Found no movies.</p>;
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
+
+  return (
+    <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>{content}</section>
+    </React.Fragment>
+  );
+}
+
+export default App;
+// alternative way of writing http get call
+
+// function fetchMoivesHandler(){
+//   // returns a promise
+//   fetch('https://swapi.dev/api/films/').then(response =>{
+//    return response.json();
+//   }).then(data =>{ 
+//     const transformedMovies =data.results.map(moviesData => {
+//       return{
+//         id : moviesData.episode_id,
+//         title :moviesData.title,
+//         openingText : moviesData.opening_crawl,
+//         releaseDate : moviesData.release_date
+//       };
+//     });
+//     setMovies(transformedMovies);
+//    } );
+// }
